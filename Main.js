3f67231b3c3db4aa744c34e1ad8e23ea56f81c4a
Jose Zapatero <jzapatero14@r2happrentice.org>
2023-10-09 10:02:00 -0400

    Code-Demo-Completion

---
+++ Main.js
@@ -0,0 +1,27 @@
+//if statement
+// const isMailSent = false;
+
+// if (isMailSent) {
+//   console.log('Mail sent to recipient');
+// }
+
+//else Statement
+const isTaskCompleted = false;
+
+// if (isTaskCompleted) {
+//   console.log('Task completed');
+// } else {
+//   console.log('Task incomplete');
+// }
+
+const size = 2000;
+
+if (size > 100) {
+  console.log('Big');
+} else if (size > 20) {
+  console.log('Medium');
+} else if (size > 4) {
+  console.log('Small');
+} else {
+  console.log('Tiny');
+}
